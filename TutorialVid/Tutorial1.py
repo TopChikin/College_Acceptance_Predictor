95606b8aa92130b7ef008b141c6d63043a65004a
TopChikin <43239703+TopChikin@users.noreply.github.com>
2019-10-06 15:16:45 -0400

    Removed HelloWorld.py Added new Directory - TutorialVid (Learn Tensorflow 2.0.0) Added new file Tutorial1.py

---
+++ TutorialVid/Tutorial1.py
@@ -0,0 +1,28 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # removes debug info
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+def idenL(index):
+    return class_names[index]
+
+
+# Grab fashion data images
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+# Shrink data to float nums
+train_images = train_images / 255.0
+train_labels = train_labels / 255.0
+
+# plt.imshow(train_images[0], cmap=plt.cm.binary) #Shows with proper colors
+# plt.imshow(train_images[0]) #Shows with weird colors
+# plt.show()
