1bf28f3165378f99fbe628ee7f70c93963baf7f1
TopChikin <43239703+TopChikin@users.noreply.github.com>
2019-10-07 00:06:26 -0400

    Programming Tutorial1

--- TutorialVid/Tutorial1.py
+++ TutorialVid/Tutorial1.py
@@ -2,7 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # removes debug info
 import tensorflow as tf
 from tensorflow import keras
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
@@ -31,11 +31,17 @@ test_images = test_images / 255.0
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(128),
     keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(train_images, train_labels, epochs=5)
 
+
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print("Tested Accuracy:", test_acc)
+
+
+prediction = model.predict(test_images)
+print(idenL(np.argmax(prediction[0])))
