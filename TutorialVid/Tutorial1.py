6368f20de41a9e72456ce2b8d340e9a005b54758
TopChikin <43239703+TopChikin@users.noreply.github.com>
2019-10-06 22:40:46 -0400

    Programming Tutorial1

--- TutorialVid/Tutorial1.py
+++ TutorialVid/Tutorial1.py
@@ -22,8 +22,20 @@ data = keras.datasets.fashion_mnist
 
 # Shrink data to float nums
 train_images = train_images / 255.0
-train_labels = train_labels / 255.0
+test_images = test_images / 255.0
 
 # plt.imshow(train_images[0], cmap=plt.cm.binary) #Shows with proper colors
 # plt.imshow(train_images[0]) #Shows with weird colors
 # plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print("Tested Accuracy:", test_acc)
