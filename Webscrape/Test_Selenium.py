1552860b7ab9b535ef418c51f9d682388d6d0f92
TopChikin <43239703+TopChikin@users.noreply.github.com>
2019-10-10 01:30:49 -0400

    Programming Selenium College Program FINALLY GOT IT TO FIND SAT SCORE, GPA, AND ACCEPTANCE/REJECTION

--- Webscrape/Test_Selenium.py
+++ Webscrape/Test_Selenium.py
@@ -3,34 +3,33 @@ from selenium import webdriver as wd
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
-#Group effort mostly by reece
+# Group effort mostly by reece
 
 with open(r'C:\Users\jonat\PycharmProjects\Python-Tensorflow\Creds\school_creds.txt', 'r') as creds_file:
     username = creds_file.readline()
     password = creds_file.readline()
 
-driver = wd.Chrome(r'C:\Users\jonat\PycharmProjects\Github-Tensorflow\VenvInstances\chromedriver.exe')
+driver = wd.Chrome()
 
-#Setup driver / engine
+# Setup driver / engine
 url = 'https://launchpad.classlink.com/loudoun%20?'
-driver.get(url) #Access website
+driver.get(url)  # Access website
 
-driver.find_element_by_name("username").send_keys(username) #Entering Username
-driver.find_element_by_name('password').send_keys(password) #Entering Username
+driver.find_element_by_name("username").send_keys(username)  # Entering Username
+driver.find_element_by_name('password').send_keys(password)  # Entering Username
 driver.find_element_by_name('signin').click()
 
-sleep(1)
+sleep(2)
 
 driver.get('https://launchpad.classlink.com/browsersso/423078')
 
-def waitForClass(class_Name):
-    while driver.find_element_by_class_name(class_Name) == None:
-        sleep(0.5)
+# def waitForClass(class_Name):
+#     while driver.find_element_by_class_name(class_Name) == None:
+#         sleep(0.5)
 
 while True:
     try:
-        butt = driver.find_element_by_class_name('js-btn-continue-to-site')
-        butt.click()
+        driver.find_element_by_class_name('js-btn-continue-to-site').click()
         break
     except:
         sleep(0.5)
@@ -39,7 +38,7 @@ driver.find_element_by_name('ctl00$ContentPlaceHolder1$UsernameTextBox').send_ke
 driver.find_element_by_name('ctl00$ContentPlaceHolder1$PasswordTextBox').send_keys(password)
 driver.find_element_by_name('ctl00$ContentPlaceHolder1$SubmitButton').click()
 
-#college = input('Input College: ')
+# college = input('Input College: ')
 college = 'virginia tech'
 
 sleep(1)
@@ -52,10 +51,10 @@ sleep(1)
 
 html = driver.page_source
 soup = BeautifulSoup(html, features="html.parser")
-collegeContainer = soup.find('td', {'class':'containers-Colleges-LookupV2-Grid-styles-column'})
-#rint(collegeContainer)
+collegeContainer = soup.find('td', {'class': 'containers-Colleges-LookupV2-Grid-styles-column'})
+# rint(collegeContainer)
 button = collegeContainer.find('a', class_='components-ClickHOC-styles-medium')
-#print(button['href'])
+# print(button['href'])
 driver.get('https://student.naviance.com' + button['href'])
 
 sleep(1)
@@ -77,6 +76,7 @@ action.move_to_element(admission)
 action.click().perform()
 """
 
+# Couldn't find button element to click, so User will manually click instead temporarily
 while True:
     try:
         html = driver.page_source
@@ -92,14 +92,39 @@ while True:
         print('Admissions Page Not Open (Please click on Admissions)')
         sleep(1)
 
-sleep(2)
+sleep(1)
 
 # html = driver.page_source
 # soup = BeautifulSoup(html, features="html.parser")
-# data_container = soup.find('g', class_='nv-groups')
-# print(data_container)
-# data_array = data_container.find_all('g', class_='nv-group')
-# print(data_array)
 
-print('CLOSING DRIVER')
-driver.close()
+"""
+OMG IT FINALLY WORKS -JONATHAN LE 1:27 AM 10/10/19
+"""
+point_container = driver.find_element_by_class_name('nv-point-paths')
+points = point_container.find_elements_by_tag_name('path')
+for point in points:
+    action = ActionChains(driver).move_to_element(point).perform()
+    html = driver.page_source
+    soup = BeautifulSoup(html, features="html.parser")
+
+    try:
+        data_point = soup.find('div', class_='xy-tooltip')
+        print(data_point)
+    except:
+        print('CANT FIND DATA POINT')
+
+
+
+
+# points = point_container.find_elements_by_tag_name('path')
+# print(points)
+# for point in points:
+#     action = ActionChains(driver).move_to_element(point).perform()
+#     sleep(1)
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, features="html.parser")
+#     data_point = soup.find('div', class_='nvtooltip xy-tooltip')
+#     print(data_point)
+
+# print('CLOSING DRIVER')
+# driver.close()
